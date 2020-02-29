13696ed6051565cae1ce828482db51a35a6b0379
peterjewicz <peterjewicz@totalwebconnections.com>
2020-02-29 09:55:59 -0600

    remove tracking built files

--- mobile/www/target/cljsbuild-compiler-1/dungeon_builder/components/textbox.cljs
+++
@@ -1,5 +0,0 @@
-(ns dungeon_builder.components.textbox)
-
-
-(defn render [text]
-  [:p (str "I display the text: " text)])
--- mobile/www/target/cljsbuild-compiler-1/dungeon_builder/services/state/textstate.cljs
+++
@@ -1,5 +0,0 @@
-(ns dungeon_builder.services.state.textstate)
-
-(defn update-state-text [app-state payload]
-  "example of updating the state based on the payload"
-  (swap! app-state conj {:text payload}))
